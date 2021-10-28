3bf48c610bc6887089b337d2a7f73ca700593142
Lawrence Splaver <lsplaver@yahoo.com>
2021-10-28 15:34:42 -0400

    Pig and PigViewModel models intially coded.

---
+++ PigGame/Models/Pig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PigGame.Models
+{
+    public class Pig
+    {
+        public int PlayerOneTotalScore { get; set; }
+
+        public int PlayerTwoTotalScore { get; set; }
+
+        public int CurrentTurnScore { get; set; }
+
+        public int CurrentPlayer { get; set; }
+    }
+}
