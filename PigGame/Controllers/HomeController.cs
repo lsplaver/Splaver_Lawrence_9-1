8a8a392cf2e03608041b484b4e2d728de2ff7c04
Lawrence Splaver <lsplaver@yahoo.com>
2021-10-28 14:49:04 -0400

    Initial coding of HomeController, Index, _Layout and _ViewImports

--- PigGame/Controllers/HomeController.cs
+++ PigGame/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using PigGame.Models;
+// using PigGame.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
