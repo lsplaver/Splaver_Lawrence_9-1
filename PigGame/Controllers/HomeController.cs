0b83f4c6df87264b9860309fb01adc594d7e0831
Lawrence Splaver <lsplaver@yahoo.com>
2021-10-28 14:13:06 -0400

    Add project files.

---
+++ PigGame/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PigGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PigGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
